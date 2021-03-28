9383b2e0a0741c801c0237412d5413e54c2a3cfa
tangshuang <mail@tangshuang.net>
2021-03-28 22:25:26 +0800

    init

---
+++ README.md
@@ -0,0 +1,153 @@
+# ABS-HTML
+
+将HTML转化为[HyperJSON](https://www.tangshuang.net/8026.html)后处理，实现HTML的无状态计算。
+
+## 安装
+
+```html
+<script src="//unpkg.com/abs-html"></script>
+<script>
+  const {
+    parseHTMLToHyperJSON,
+    rebuildHyperJSONToHTML,
+    diffHyperJSON,
+    patchHyperJSON,
+  } = window
+</script>
+```
+
+```sh
+npm i abs-html
+```
+
+```js
+import {
+  parseHTMLToHyperJSON,
+  rebuildHyperJSONToHTML,
+  diffHyperJSON,
+  patchHyperJSON,
+} from 'abs-html'
+```
+
+## 使用
+
+### parseHTMLToHyperJSON(html, { loose })
+
+- html HTML字符串，注意，只能包含一个根节点
+- loose 是否宽松，开启后，将保留标签换行（和原始html换行一致）
+
+```js
+const hyperJSON = parseHTMLToHyperJSON(`
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <title>xxx</title>
+    </head>
+    <body>
+      <-- comment -->
+      <div class="dog">content</div>
+      <div id="xx">111</div>
+      <div class="cat">cat</div>
+      <img src="http://xxx.com/a.jpg" />
+      <div></div>
+      <my-app />
+    </body>
+  </html>
+`)
+
+/**
+[
+    "!DOCTYPE",
+    {
+        "html": null
+    },
+    [
+        "html",
+        null,
+        [
+            "head",
+            null,
+            [
+                "title",
+                null,
+                "xxx"
+            ]
+        ],
+        [
+            "body",
+            null,
+            [
+                "#comment",
+                null,
+                " comment "
+            ],
+            [
+                "div",
+                {
+                    "class": "dog"
+                },
+                "content"
+            ],
+            [
+                "div",
+                {
+                    "id": "xx"
+                },
+                "111"
+            ],
+            [
+                "div",
+                {
+                    "class": "cat"
+                },
+                "cat"
+            ],
+            [
+                "img",
+                {
+                    "src": "http://xxx.com/a.jpg"
+                }
+            ],
+            [
+                "div",
+                null
+            ],
+            [
+                "my-app",
+                null
+            ]
+        ]
+    ]
+]
+*/
+```
+
+### rebuildHyperJSONToHTML(hyperJSON)
+
+```js
+const html = rebuildHyperJSONToHTML(hyperJSON)
+
+/**
+<!DOCTYPE html><html>...</html>
+*/
+```
+
+注意，恢复出来的html是否紧凑，完全由创建hyperJSON时传入的loose参数决定。
+
+### diffHyperJSON(hyperJSON1, hyperJSON2)
+
+查看hyperJSON2相对于hyperJSON1而言，有哪些变化。
+
+```js
+const mutations = diffHyperJSON(hyperJSON1, hyperJSON2)
+```
+
+### patchHyperJSON(hyperJSON, mutations)
+
+将mutations作用于一个hyperJSON，得到一个新的经过改变的hyperJSON。
+
+```js
+const hyperJSON2 = patchHyperJSON(hyperJSON, mutations)
+```
+
+注意，由于diff算法的一些局限性，loose参数为true时，可能无法准确还原换行。
---
+++ index.js
@@ -0,0 +1,572 @@
+/**
+ * 将html字符串解析为hyperJSON
+ * @param {string} html
+ * @param {object} options
+ * @param {boolean} options.loose 是否宽松，开启后，将保留标签换行（和原始html换行一致）
+ * @returns hyperJSON
+ */
+function parseHTMLToHyperJSON(html, options = {}) {
+  const nest = []
+
+  const len = html.length
+
+  let inTagBegin = null
+  let inTag = null
+
+  const nodes = []
+
+  for (let i = 0; i < len; i ++) {
+    let char = html[i]
+    const next = html[i + 1]
+    if (inTag && char === '<' && next === '/') {
+      while (char !== '>') {
+        i ++
+        char = html[i]
+      }
+
+      // TODO check intag
+
+      const node = inTag
+      if (node.length < 3) {
+        node[1] = node[1] || null
+        node[2] = ''
+      }
+
+      nest.pop()
+      inTag = nest[nest.length - 1]
+    }
+    else if (!inTagBegin && char === '<') {
+      if (html[i + 1] === '-' && html[i + 2] === '-') {
+        const comment = ['#comment', null]
+        let content = ''
+
+        i += 3
+        char = html[i]
+
+        while (!(char === '-' && html[i + 1] === '-' && html[i + 2] === '>')) {
+          content += char
+
+          i ++
+          char = html[i]
+        }
+
+        comment[2] = content
+        const parent = nest.length ? nest[nest.length - 1] : nest
+        parent.push(comment)
+
+        i += 2
+        continue
+      }
+
+      let tag = ''
+
+      i ++
+      char = html[i]
+
+      while (char !== ' ' && char !== '>') {
+        tag += char
+
+        i ++
+        char = html[i]
+      }
+
+      const node = [tag]
+      inTagBegin = node
+      nodes.push(node)
+
+      i --
+    }
+    else if (inTagBegin && char === ' ') {
+      let attr = ''
+
+      i ++
+      char = html[i]
+
+      while (char !== '=' && char !== ' ' && char !== '>') {
+        if (char === '/' && html[i + 1] === '>') {
+          break
+        }
+
+        attr += char
+
+        i ++
+        char = html[i]
+      }
+
+      const node = inTagBegin
+
+      if (char === '/' && html[i + 1] === '>') {
+        const parent = nest.length ? nest[nest.length - 1] : nest
+        parent.push(node)
+        inTagBegin = null
+        i ++
+        continue
+      }
+
+      if (char === '=') {
+        let value = ''
+
+        i ++
+        char = html[i]
+
+        while (char !== ' ' && char !== '>') {
+          if (char === '/' && html[i + 1] === '>') {
+            break
+          }
+
+          value += char
+
+          i ++
+          char = html[i]
+        }
+
+        if (char === '/' && html[i + 1] === '>') {
+          const parent = nest.length ? nest[nest.length - 1] : nest
+          parent.push(node)
+          inTagBegin = null
+          i ++
+          continue
+        }
+
+        if (value[0] === '"' && value[value.length - 1] === '"') {
+          value = value.substring(1, value.length - 1)
+        }
+        else if (value[0] === "'" && value[value.length - 1] === "'") {
+          value = value.substring(1, value.length - 1)
+        }
+
+        node[1] = node[1] || {}
+        node[1][attr] = value
+      }
+      else if (char === ' ' || char === '>') {
+        node[1] = node[1] || {}
+        node[1][attr] = null
+      }
+
+      i --
+    }
+    else if (inTagBegin && char === '>') {
+      const node = inTagBegin
+      const parent = nest.length ? nest[nest.length - 1] : nest
+      parent.push(node)
+      nest.push(node)
+      inTagBegin = null
+      inTag = node
+    }
+    else if (inTag) {
+      const node = inTag
+      if (node.length < 3) {
+        node[1] = node[1] || null
+        node[2] = char
+      }
+      else if (typeof node[node.length - 1] === 'string') {
+        node[node.length - 1] += char
+      }
+      else {
+        node.push(char)
+      }
+    }
+  }
+
+  /**
+   * 检查内容是否包含字符串，如果不不包含字符串，说明这里的换行没有意义，是代码层面的，需要清除，只留下子标签
+   */
+  if (!options.loose) {
+    nodes.forEach((node) => {
+    const [tag, attrs, ...children] = node
+    const strs = children.filter(item => typeof item === 'string' && item.trim())
+      if (!strs.length) {
+        const contents = children.filter(item => typeof item !== 'string')
+        node.length = 2
+        node.push(...contents)
+      }
+    })
+  }
+
+  return nest[0]
+}
+
+function rebuildHyperJSONToHTML(hyperjson) {
+  const [name, attrs, ...children] = hyperjson
+
+  let html = ''
+
+  const buildAttrs = (attrs) => {
+    let str = ''
+
+    if (!attrs) {
+      return str
+    }
+
+    const keys = Object.keys(attrs)
+    keys.forEach((key) => {
+      const value = attrs[key]
+      if (value === null) {
+        str += ` ${key}`
+      }
+      else {
+        str += ` ${key}="${value.replace(/\\"/gm, '"').replace(/"/gm, '\\"')}"`
+      }
+    })
+
+    return str
+  }
+  const buildChildren = (children) => {
+    let str = ''
+
+    if (!children || !children.length) {
+      return str
+    }
+
+    children.forEach((child) => {
+      if (typeof child === 'string') {
+        str += child
+      }
+      else {
+        str += rebuildHyperJSONToHTML(child)
+      }
+    })
+
+    return str
+  }
+
+  if (name.indexOf('!') === 0) {
+    html += `<${name}${buildAttrs(attrs)}>${buildChildren(children)}`
+  }
+  else if (name.indexOf('#') === 0) {
+    if (name === '#comment') {
+      html += `<--${children[0]}-->`
+    }
+  }
+  else if (children.length) {
+    html += `<${name}${buildAttrs(attrs)}>${buildChildren(children)}</${name}>`
+  }
+  else {
+    html += `<${name}${buildAttrs(attrs)} />`
+  }
+
+  return html
+}
+
+function diffHyperJSON(hyperjson1, hyperjson2) {
+  const getIdentifiers = (items) => {
+    const ids = items.map((item) => {
+      if (typeof item === 'string') {
+        return '#text'
+      }
+      else {
+        const [name, attrs] = item
+        if (name.indexOf('#') === 0) {
+          return name
+        }
+        else if (attrs && attrs.id) {
+          return `${name}#${attrs.id}`
+        }
+        else {
+          return `${name}${attrs ? `[${Object.keys(attrs).join(',')}]` : ''}`
+        }
+      }
+    })
+    const res = ids.map((id, i) => {
+      const regression = ids.slice(0, i)
+      const count = regression.filter(item => item === id).length
+      return id + '@' + (count + 1)
+    })
+    return res
+  }
+
+  const makePath = (item, items) => {
+    let nth = 1
+
+    for (let i = 0, len = items.length; i < len; i ++) {
+      if (items[i] === item) {
+        break
+      }
+      if (typeof item === 'string') {
+        if (typeof items[i] === 'string') {
+          nth ++
+        }
+      }
+      else if (items[i][0] === item[0]) {
+        nth ++
+      }
+    }
+
+    const name = typeof item === 'string' ? 'text()'
+      : item[0].indexOf('#') === 0 ? item[0].replace('#', '') + '()'
+      : item[0]
+
+    return `${name}[${nth}]`
+  }
+
+  const createXPath = (deepth, path) => {
+    return deepth.concat(path).join('/')
+  }
+
+  const diffAttrs = (attrs1, attrs2, deepth) => {
+    const _attrs = { ...(attrs1 || {}), ...(attrs2 || {}) }
+    const keys = Object.keys(_attrs)
+    const mutations = []
+    keys.forEach((key) => {
+      if (attrs1[key] !== attrs2[key]) {
+        mutations.push({
+          type: 'attribute',
+          target: createXPath(deepth),
+          name: key,
+          next: key in attrs2 ? attrs2[key] : null,
+          prev: key in attrs1 ? attrs1[key] : null,
+        })
+      }
+    })
+    return mutations
+  }
+
+  const diff = (items1, items2, deepth) => {
+    const identifiers1 = getIdentifiers(items1)
+    const identifiers2 = getIdentifiers(items2)
+
+    const memoItems = [...items1]
+    const memoIdentifiers = [...identifiers1]
+
+    const removed = []
+    const inserted = []
+    const moved = []
+
+    for (let i = 0; i < items1.length; i ++) {
+      const id1 = identifiers1[i]
+
+      const index = identifiers2.indexOf(id1)
+      if (index < 0) {
+        removed.push({
+          node: makePath(items1[i], items1),
+        })
+
+        const index = memoIdentifiers.indexOf(id1)
+        memoItems.splice(index, 1)
+        memoIdentifiers.splice(index, 1)
+      }
+    }
+
+    for (let i = 0, len = items2.length - 1; i < len; i ++) {
+      const id2 = identifiers2[i]
+
+      const index = memoIdentifiers.indexOf(id2)
+      if (index < 0) {
+        let sibling = null
+        for (let curr = i + 1, len = identifiers2.length; curr < len; curr ++) {
+          const id2 = identifiers2[curr]
+          const index = memoIdentifiers.indexOf(id2)
+          if (index > -1) {
+            sibling = index
+            break
+          }
+        }
+
+        const before = sibling === null ? null : makePath(memoItems[sibling], memoItems)
+        const item = items2[i]
+        const node = {
+          before,
+          next: item,
+        }
+
+        inserted.push(node)
+        memoItems.splice(sibling ? sibling : 0, 0, item)
+        memoIdentifiers.splice(sibling ? sibling : 0, 0, id2)
+      }
+    }
+
+    for (let i = items2.length - 1; i > -1; i --) {
+      const id2 = identifiers2[i]
+
+      const index = memoIdentifiers.indexOf(id2)
+      if (index > -1 && index !== i) {
+        const item = memoItems[index]
+        const node = makePath(item, memoItems)
+        memoItems.splice(index, 1)
+        memoIdentifiers.splice(index, 1)
+
+        const next = i + 1
+        let before = null
+        if (next >= items2.length) {
+          memoItems.push(item)
+          memoIdentifiers.push(id2)
+        }
+        else {
+          const nextId = identifiers2[next]
+          const nextIndex = memoIdentifiers.indexOf(nextId)
+          const nextItem = memoItems[nextIndex]
+          before = makePath(nextItem, memoItems)
+
+          memoItems.splice(i, 0, item)
+          memoIdentifiers.splice(i, 0, id2)
+        }
+
+        moved.push({
+          before,
+          node,
+        })
+      }
+    }
+
+    const mutation = {
+      type: 'children',
+      target: createXPath(deepth, []),
+      removed,
+      inserted,
+      moved,
+    }
+
+    const mutations = []
+
+    if (mutation.removed.length + mutation.inserted.length + mutation.moved.length) {
+      mutations.push(mutation)
+    }
+
+    for (let i = 0, len = memoItems.length; i < len; i ++) {
+      const item = memoItems[i]
+      const node = makePath(item, memoItems)
+
+      if (typeof item === 'string') {
+        if (item !== items2[i]) {
+          mutations.push({
+            type: 'text',
+            target: createXPath(deepth, node),
+            next: items2[i],
+            prev: item,
+          })
+        }
+      }
+      else if (items1.includes(item)) {
+        const [_name1, attrs1, ...children1] = item
+        const [_name2, attrs2, ...children2] = items2[i]
+
+        const attrsMutations = diffAttrs(attrs1, attrs2, [...deepth, node])
+        mutations.push(...attrsMutations)
+
+        const changes = diff(children1, children2, [...deepth, node])
+        mutations.push(...changes)
+      }
+    }
+
+    return mutations
+  }
+
+
+
+  const mutations = []
+  const [name1, attrs1, ...children1] = hyperjson1
+  const [name2, attrs2, ...children2] = hyperjson2
+
+  const attrsMutations = diffAttrs(attrs1, attrs2, [])
+  mutations.push(...attrsMutations)
+
+  const childrenMutations = diff(children1, children2, [])
+  mutations.push(...childrenMutations)
+
+  return mutations
+}
+
+function patchHyperJSON(hyperjson, mutations) {
+  const deepClone = (obj) => {
+    const copy = Array.isArray(obj) ? [] : {}
+    for (let key in obj) {
+      if (Object.prototype.hasOwnProperty.call(obj, key)) {
+         copy[key] = obj[key] && typeof obj[key] === 'object' ? deepClone(obj[key]) : obj[key]
+      }
+    }
+    return copy
+  }
+  const findNode = (hyperjson, target) => {
+    if (target === null) {
+      const [_name, _attrs, ...children] = hyperjson
+      return [null, children.length, hyperjson]
+    }
+
+    const path = target.split('/').map((item) => {
+      const name = item.replace(/\[.*?\]$/, '')
+      const nth = +item.replace(name, '').replace(/[\[\]]/g, '')
+      return [name, nth]
+    })
+
+    let node = hyperjson
+    let parent = null
+    let index = -1
+    path.forEach(([name, nth]) => {
+      const [_name, _attrs, ...children] = node
+
+      let sibling = 0
+      for (let i = 0, len = children.length; i < len; i ++) {
+        const child = children[i]
+        if (typeof child === 'string') {
+          if (name === 'text()') {
+            sibling ++
+          }
+        }
+        else if (name === child[0]) {
+          sibling ++
+        }
+        else if (name.substr(name.length - 2, 2) === '()' && child[0].indexOf('#') === 0 && name.substring(0, name.length - 2) === child[0].substr(1)) {
+          sibling ++
+        }
+
+        if (sibling === nth) {
+          parent = node
+          node = child
+          index = i
+          return
+        }
+      }
+    })
+
+    return [node, index, parent]
+  }
+
+  const json = deepClone(hyperjson)
+  mutations.forEach((mutation) => {
+    const { type, target } = mutation
+    const [node, index, parent] = findNode(json, target)
+
+    if (type === 'text') {
+      const { next } = mutation
+      parent[index + 2] = next
+    }
+    else if (type === 'attribute') {
+      const { name, next } = mutation
+      const attrs = node[1] || {}
+      attrs[name] = next
+      node[1] = attrs
+    }
+    else if (type === 'children') {
+      const { removed, inserted, moved } = mutation
+      removed.forEach((item) => {
+        const { node: path } = item
+        const [_, index, parent] = findNode(node, path)
+        parent.splice(index + 2, 1)
+      })
+      inserted.forEach((item) => {
+        const { before, next } = item
+        const [_, index, parent] = findNode(node, before)
+        parent.splice(index + 2, 0, next)
+      })
+      moved.forEach((item) => {
+        const { node: path, before } = item
+
+        const [next, removeIndex, removeFromParent] = findNode(node, path)
+        removeFromParent.splice(removeIndex + 2, 1)
+
+        const [_, index, parent] = findNode(node, before)
+        parent.splice(index + 2, 0, next)
+      })
+    }
+  })
+  return json
+}
+
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = {
+    parseHTMLToHyperJSON,
+    rebuildHyperJSONToHTML,
+    diffHyperJSON,
+    patchHyperJSON,
+  }
+}
